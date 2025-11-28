f2c10c85c6b0a6202630d4c4270cc072afb9125c
Rj lama <riteshlama5@gmail.com>
2025-11-28 12:39:33 +1100

    firebase

--- CookBook/CookBookApp.swift
+++ CookBook/CookBookApp.swift
@@ -5,9 +5,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
+
 
 @main
 struct CookBookApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var authManager: AuthManager = AuthManager()
     var body: some Scene {
         WindowGroup {
